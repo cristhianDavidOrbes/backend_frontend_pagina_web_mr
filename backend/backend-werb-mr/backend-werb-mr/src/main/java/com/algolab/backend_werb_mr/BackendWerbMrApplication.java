6fb05565dea4027625fc0b67ceae758a9663aac1
cristhianDavidOrbes <cristhian.orbes@campusucc.edu.co>
2026-04-22 11:00:37 -0500

    implementacion de estructura del backend

---
+++ backend/backend-werb-mr/backend-werb-mr/src/main/java/com/algolab/backend_werb_mr/BackendWerbMrApplication.java
@@ -0,0 +1,13 @@
+package com.algolab.backend_werb_mr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWerbMrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWerbMrApplication.class, args);
+	}
+
+}
---
+++ backend/backend-werb-mr/backend-werb-mr/src/test/java/com/algolab/backend_werb_mr/BackendWerbMrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algolab.backend_werb_mr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWerbMrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
